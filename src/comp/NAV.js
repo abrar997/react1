ae189f28e367cfd5be2769c1243689d7440f067f
abrar997 <abraralrawi997@gmail.com>
2021-09-06 21:20:30 +0300

    react1 project all files

--- src/comp/NAV.js
+++ src/comp/NAV.js
@@ -6,7 +6,9 @@ class NAV extends Component {
     return (
         <div> <div className="container"> <div className="row">
             <nav className="navbar navbar-expand-lg  bg-light">
-                <a className="navbar-brand" ><img src="images\logo.webp"     /></a>
+                <a className="navbar-brand" href="#">
+                    code/<span>Mu</span> <span>.</span>{" "}
+                </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
